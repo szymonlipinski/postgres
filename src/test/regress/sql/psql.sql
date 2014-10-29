6d1e548d8ddf231aac716c4a834b73e65cc3139b
Szymon Guz <simon@simononsoftware.com>
2014-10-29 12:07:33 +0100

    Fix tests.

--- src/test/regress/sql/psql.sql
+++ src/test/regress/sql/psql.sql
@@ -277,7 +277,7 @@ execute q;
 
 deallocate q;
 
-prepare q as select 'lkjsafi\\/ /oeu rio)(!@&*#)*(!&@*) \ (&' as "012345678 9abc def!*@#&!@(*&*~~_+-=\ \", '11' as "0123456789", 11 as int from generate_series(1,10) as n;
+prepare q as select ' | = | lkjsafi\\/ /oeu rio)(!@&*#)*(!&@*) \ (&' as " | -- | 012345678 9abc def!*@#&!@(*&*~~_+-=\ \", '11' as "0123456789", 11 as int from generate_series(1,10) as n;
 
 \pset format asciidoc
 \pset expanded off
